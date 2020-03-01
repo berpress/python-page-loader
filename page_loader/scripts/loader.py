c5cecf26ea65d1e1e06fe6d935efaaddc41dfc0a
berpress <berpress@gmail.com>
2020-03-01 22:47:41 +0400

    Add progress bar

--- page_loader/scripts/loader.py
+++ page_loader/scripts/loader.py
@@ -3,6 +3,7 @@ import argparse
 import logging.config
 from os import path
 import sys
+from progress.bar import Bar
 
 from page_loader.utils.client import get_response, check_response_answer
 from page_loader.utils.logger import run_logger
@@ -28,21 +29,31 @@ def main():
         logging_level = 20
     logger.setLevel(logging_level)
     logger.debug('Set logging level %s (NOTSET 0, CRITICAL 50)', logging_level)
-    response = get_response(url)
-    logger.debug('Output file is %s, download from %s', output_file, url)
-    status_code = response.status_code
-    if not check_response_answer(status_code):
-        logger.error('Error, because status code is %s', status_code)
-        sys.exit(1)
-    logger.debug('Status code is 200, GET %s', url)
-    file_name = get_file_name_from_url(url)
-    path_to_file = path.join(output_file, file_name)
-    edited_html = set_local_links(response.text, path_to_file, url)
-    write_data_to_file(edited_html, file_name=file_name,
-                       file_dir=output_file)
-    logger.info('Finish write data to %s file in %s folder', file_name,
-                output_file)
-    sys.exit(0)
+    with Bar('Processing', max=10) as bar:
+        for i in range(10):
+            bar.next()
+            response = get_response(url)
+            bar.next(4)
+            logger.debug('Output file is %s, download from %s', output_file,
+                         url)
+            status_code = response.status_code
+            if not check_response_answer(status_code):
+                logger.error('Error, because status code is %s', status_code)
+                sys.exit(1)
+            logger.debug('Status code is 200, GET %s', url)
+            bar.next()
+            file_name = get_file_name_from_url(url)
+            path_to_file = path.join(output_file, file_name)
+            bar.next()
+            edited_html = set_local_links(response.text, path_to_file, url)
+            bar.next()
+            write_data_to_file(edited_html, file_name=file_name,
+                               file_dir=output_file)
+            bar.next()
+            bar.finish()
+            logger.info('Finish write data to %s file in %s folder', file_name,
+                        output_file)
+            sys.exit(0)
 
 
 if __name__ == '__main__':
